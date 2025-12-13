3bacdcb43534adaa2b2a7624bf89e144e50c752b
Anderson Laventure <Laventurepsu1@gmail.com>
2025-12-12 23:43:20 -0500

    increase limit

--- src/app/api/advocates/paginated/route.ts
+++ src/app/api/advocates/paginated/route.ts
@@ -4,7 +4,7 @@ import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page = parseInt(searchParams.get("page") || "0");
-  const limit = parseInt(searchParams.get("limit") || "4");
+  const limit = parseInt(searchParams.get("limit") || "8");
   const search = searchParams.get("search") || "";
 
   let data = advocateData;
