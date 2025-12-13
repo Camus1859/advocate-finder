0f12f2fb35a5f0b898b178c32c51661eb286a7ed
Anderson Laventure <Laventurepsu1@gmail.com>
2025-12-12 23:43:41 -0500

    refactor

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,9 +1,7 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function POST() {
-  const records = await db.insert(advocates).values(advocateData).returning();
+  const records = advocateData;
 
   return Response.json({ advocates: records });
 }
